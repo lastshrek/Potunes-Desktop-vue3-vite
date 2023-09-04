e8c0b30cfb5d27d88b1d85eb6ece000f81cc527f
Purchas Raul <lastshrek@foxmail.com>
2023-09-04 23:04:12 +0800

    fix array type

--- src/api/index.ts
+++ src/api/index.ts
@@ -2,7 +2,7 @@
  * @Author       : lastshrek
  * @Date         : 2023-09-02 17:09:39
  * @LastEditors  : lastshrek
- * @LastEditTime : 2023-09-02 23:40:18
+ * @LastEditTime : 2023-09-04 19:57:58
  * @FilePath     : /src/api/index.ts
  * @Description  : api
  * Copyright 2023 lastshrek, All Rights Reserved.
@@ -14,4 +14,4 @@ export const home = () => get('/playlists/home')
 // 获取网易云音乐热门新碟
 export const neteaseTopAlbum = () => get('/netease/top_album')
 // 获取网易云每日推荐歌单
-export const neteaseRecommendDaily = (data: string) => post('/netease/recommend_daily', data)
+export const neteaseRecommendDaily = (data: { cookie: string }) => post('/netease/recommend_daily', data)
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,17 +2,17 @@
  * @Author       : lastshrek
  * @Date         : 2023-09-01 21:16:34
  * @LastEditors  : lastshrek
- * @LastEditTime : 2023-09-03 01:22:01
+ * @LastEditTime : 2023-09-04 19:58:42
  * @FilePath     : /src/views/Home.vue
  * @Description  : Home
  * Copyright 2023 lastshrek, All Rights Reserved.
  * 2023-09-01 21:16:34
 -->
 <template>
-  <div class="md:flex flex-col md:flex-row min-h-screen w-full shadow bg-black pt-14 pb-16">
-    <div class="w-full px-4 pt-2">
-      <!-- Mine -->
-      <!-- <div v-show="isUser">
+	<div class="md:flex flex-col md:flex-row min-h-screen w-full shadow bg-black pt-14 pb-16">
+		<div class="w-full px-4 pt-2">
+			<!-- Mine -->
+			<!-- <div v-show="isUser">
         <HeaderTitle title="我的" :showmore="false"></HeaderTitle>
         <div class="mt-4 md:grid md:grid-cols-5 md:gap-4 flex flex-wrap">
           <AlbumCard name="我的收藏" :cover_url="heartSrc" max_width="md:max-w-md"
@@ -20,80 +20,110 @@
           </AlbumCard>
         </div>
       </div> -->
-      <!-- collections -->
-      <HeaderTitle title="月度精选" @click.native="pushToPlaylistDetail('collections')"></HeaderTitle>
-      <div class="mt-4 md:flex">
-        <AlbumCard v-for="collection in collections" :key="collection.id" :name="collection.title"
-          :cover_url="collection.cover" max_width="md:max-w-md" @click.native="toPlaylist(collection.id)">
-        </AlbumCard>
-      </div>
-      <!-- finals -->
-      <HeaderTitle title="年终精选" @click.native="pushToPlaylistDetail('finals')"></HeaderTitle>
-      <div class="mt-4 md:grid md:grid-cols-5 md:gap-4 flex flex-wrap">
-        <AlbumCard v-for="final in finals" :key="final.id" :name="final.title" :cover_url="final.cover"
-          @click.native="toPlaylist(final.id)">
-        </AlbumCard>
-      </div>
-      <!-- netease daily -->
-      <div v-show="netease_daily.length !== 0">
-        <HeaderTitle title="网易日推" :showmore="false"></HeaderTitle>
-        <div class="mt-4 md:grid md:grid-cols-5 md:gap-4 flex flex-wrap">
-          <div
-            class="text-start relative w-full md:mr-2 mb-2 bg-albumcard rounded-lg p-4 shadow-lg hover:bg-albumcardhover text-gray-50 cursor-pointer"
-            @click="toPlaylist(0, 'netease-daily-tracks')">
-            <div class="mx-auto mb-4 w-full rounded-lg relative text-center">
-              <img :src="dailySrc" class="object-cover w-full" />
-              <div class="absolute inset-0 flex items-center justify-center">
-                <p class="text-gray-800 text-center text-3xl mt-2 font-semibold">
-                  {{ today }}
-                </p>
-              </div>
-            </div>
-            <h5 class="mb-2 text-xs font-medium overflow-hidden whitespace-nowrap overflow-ellipsis">
-              每日歌曲推荐
-            </h5>
-          </div>
-          <AlbumCard v-for="playlist in netease_daily" :key="playlist.id" :name="playlist.title"
-            :cover_url="playlist.cover" @click.native="toPlaylist(playlist.id, 'netease-daily')">
-          </AlbumCard>
-        </div>
-      </div>
-      <!-- local albums -->
-      <HeaderTitle title="站内专辑" @click.native="pushToPlaylistDetail('albums')"></HeaderTitle>
-      <div class="mt-4 md:grid md:grid-cols-5 md:gap-4 flex flex-wrap">
-        <AlbumCard v-for="album in albums" :key="album.id" :name="album.title" :cover_url="album.cover"
-          @click.native="toPlaylist(album.id)">
-        </AlbumCard>
-      </div>
-      <!-- toplist -->
-      <HeaderTitle title="网易榜单" :showmore="false"></HeaderTitle>
-      <div class="mt-4 md:grid md:grid-cols-5 md:gap-4 flex flex-wrap">
-        <AlbumCard v-for="list in netease_toplist" :key="list.id" :name="list.title" :cover_url="list.cover"
-          @click.native="toPlaylist(list.nId, 'netease')">
-        </AlbumCard>
-      </div>
-      <HeaderTitle title="热门新碟" @click.native="pushToPlaylistDetail('albums')"></HeaderTitle>
-      <div class="mt-4 md:grid md:grid-cols-5 md:gap-4 flex flex-wrap">
-        <AlbumCard v-for="album in netease_topalbums" :key="album.id" :name="album.name" :cover_url="album.picUrl"
-          :artist="album.artist.name" @click.native="toPlaylist(album.id, 'netease-album')">
-        </AlbumCard>
-      </div>
-    </div>
-    <loading :active="isLoading" :can-cancel="true" :is-full-page="true" background-color="rgba(0, 0, 0, 0.5)"
-      color="#EC4899" />
-  </div>
+			<!-- collections -->
+			<HeaderTitle title="月度精选" @click.native="pushToPlaylistDetail('collections')"></HeaderTitle>
+			<div class="mt-4 md:flex">
+				<AlbumCard
+					v-for="collection in collections"
+					:key="collection.id"
+					:name="collection.title"
+					:cover_url="collection.cover"
+					max_width="md:max-w-md"
+					@click.native="toPlaylist(collection.id)"
+				></AlbumCard>
+			</div>
+			<!-- finals -->
+			<HeaderTitle title="年终精选" @click.native="pushToPlaylistDetail('finals')"></HeaderTitle>
+			<div class="mt-4 md:grid md:grid-cols-5 md:gap-4 flex flex-wrap">
+				<AlbumCard
+					v-for="final in finals"
+					:key="final.id"
+					:name="final.title"
+					:cover_url="final.cover"
+					@click.native="toPlaylist(final.id)"
+				></AlbumCard>
+			</div>
+			<!-- netease daily -->
+			<div v-show="netease_daily.length !== 0">
+				<HeaderTitle title="网易日推" :showmore="false"></HeaderTitle>
+				<div class="mt-4 md:grid md:grid-cols-5 md:gap-4 flex flex-wrap">
+					<div
+						class="text-start relative w-full md:mr-2 mb-2 bg-albumcard rounded-lg p-4 shadow-lg hover:bg-albumcardhover text-gray-50 cursor-pointer"
+						@click="toPlaylist(0, 'netease-daily-tracks')"
+					>
+						<div class="mx-auto mb-4 w-full rounded-lg relative text-center">
+							<img :src="dailySrc" class="object-cover w-full" />
+							<div class="absolute inset-0 flex items-center justify-center">
+								<p class="text-gray-800 text-center text-3xl mt-2 font-semibold">
+									{{ today }}
+								</p>
+							</div>
+						</div>
+						<h5 class="mb-2 text-xs font-medium overflow-hidden whitespace-nowrap overflow-ellipsis">每日歌曲推荐</h5>
+					</div>
+					<AlbumCard
+						v-for="playlist in netease_daily"
+						:key="playlist.id"
+						:name="playlist.title"
+						:cover_url="playlist.cover"
+						@click.native="toPlaylist(playlist.id, 'netease-daily')"
+					></AlbumCard>
+				</div>
+			</div>
+			<!-- local albums -->
+			<HeaderTitle title="站内专辑" @click.native="pushToPlaylistDetail('albums')"></HeaderTitle>
+			<div class="mt-4 md:grid md:grid-cols-5 md:gap-4 flex flex-wrap">
+				<AlbumCard
+					v-for="album in albums"
+					:key="album.id"
+					:name="album.title"
+					:cover_url="album.cover"
+					@click.native="toPlaylist(album.id)"
+				></AlbumCard>
+			</div>
+			<!-- toplist -->
+			<HeaderTitle title="网易榜单" :showmore="false"></HeaderTitle>
+			<div class="mt-4 md:grid md:grid-cols-5 md:gap-4 flex flex-wrap">
+				<AlbumCard
+					v-for="list in netease_toplist"
+					:key="list.id"
+					:name="list.title"
+					:cover_url="list.cover"
+					@click.native="toPlaylist(list.nId, 'netease')"
+				></AlbumCard>
+			</div>
+			<HeaderTitle title="热门新碟" @click.native="pushToPlaylistDetail('albums')"></HeaderTitle>
+			<div class="mt-4 md:grid md:grid-cols-5 md:gap-4 flex flex-wrap">
+				<AlbumCard
+					v-for="album in netease_topalbums"
+					:key="album.id"
+					:name="album.name"
+					:cover_url="album.picUrl"
+					:artist="album.artist.name"
+					@click.native="toPlaylist(album.id, 'netease-album')"
+				></AlbumCard>
+			</div>
+		</div>
+		<loading
+			:active="isLoading"
+			:can-cancel="true"
+			:is-full-page="true"
+			background-color="rgba(0, 0, 0, 0.5)"
+			color="#EC4899"
+		/>
+	</div>
 </template>
 
 <script setup lang="ts">
-import { ref, onMounted } from 'vue'
+import { ref, onMounted, Ref } from 'vue'
 import { home, neteaseTopAlbum, neteaseRecommendDaily } from '@/api/index'
 import { handlePromise, isUserLogin, getCurrentDate } from '@/utils/index'
-import { useToast } from 'vue-toast-notification';
+import { useToast } from 'vue-toast-notification'
 import { useRouter, useRoute } from 'vue-router'
-import Loading from 'vue-loading-overlay';
+import Loading from 'vue-loading-overlay'
 import HeaderTitle from '@/components/headertitle/HeaderTitle.vue'
 import AlbumCard from '@/components/albumcard/AlbumCard.vue'
-import 'vue-toast-notification/dist/theme-sugar.css';
+import 'vue-toast-notification/dist/theme-sugar.css'
 // 本地图片
 import dailySrc from '@/assets/images/daily.png'
 
@@ -103,151 +133,144 @@ const today = getCurrentDate()
 const isLoading = ref(true)
 // 用户是否登录
 const isUser = isUserLogin()
-const collections = ref([]) as [Playlist]
-const finals = ref([]) as [Playlist]
-const albums = ref([]) as [Playlist]
-const netease_toplist = ref([]) as [Playlist]
+const collections: Ref<Playlist[]> = ref([])
+const finals: Ref<Playlist[]> = ref([])
+const albums: Ref<Playlist[]> = ref([])
+const netease_toplist: Ref<Playlist[]> = ref([])
 // router
 const router = useRouter()
 const route = useRoute()
 // gethome
 const homedata = async () => {
-  try {
-    const [res, err] = await handlePromise(home())
-    if (err) {
-      useToast().open({
-        message: err.message || '获取首页数据失败',
-        type: 'error',
-        duration: 3000,
-        dismissible: true,
-        position: 'top-right'
-      })
-      return
-    }
-    const result = res.data
-    collections.value = result.collections
-    finals.value = result.finals
-    albums.value = result.albums
-    netease_toplist.value = result.netease_toplist
-  } catch (error) {
-    useToast().open({
-      message: error.message,
-      type: 'error',
-      duration: 3000,
-      dismissible: true,
-      position: 'top-right'
-    })
-  } finally {
-    isLoading.value = false
-  }
+	try {
+		const [res, err] = await handlePromise(home())
+		if (err) {
+			useToast().open({
+				message: err.message || '获取首页数据失败',
+				type: 'error',
+				duration: 3000,
+				dismissible: true,
+				position: 'top-right',
+			})
+			return
+		}
+		const result = res.data
+		collections.value = result.collections
+		finals.value = result.finals
+		albums.value = result.albums
+		netease_toplist.value = result.netease_toplist
+	} catch (error) {
+		useToast().open({
+			message: error.message,
+			type: 'error',
+			duration: 3000,
+			dismissible: true,
+			position: 'top-right',
+		})
+	} finally {
+		isLoading.value = false
+	}
 }
-const netease_topalbums = ref([]) as [Netease_Album]
+const netease_topalbums: Ref<Netease_Album[]> = ref([])
 // get netease top albums
 const getNeteaseTopAlbums = async () => {
-  try {
-    const [res, err] = await handlePromise(neteaseTopAlbum())
-    if (err) {
-      useToast().open({
-        message: err.message || '获取网易热门新碟失败',
-        type: 'error',
-        duration: 3000,
-        dismissible: true,
-        position: 'top-right'
-      })
-      return
-    }
-    netease_topalbums.value = res.data
-  } catch (error) {
-    useToast().open({
-      message: error.message,
-      type: 'error',
-      duration: 3000,
-      dismissible: true,
-      position: 'top-right'
-    })
-  }
+	try {
+		const [res, err] = await handlePromise(neteaseTopAlbum())
+		if (err) {
+			useToast().open({
+				message: err.message || '获取网易热门新碟失败',
+				type: 'error',
+				duration: 3000,
+				dismissible: true,
+				position: 'top-right',
+			})
+			return
+		}
+		netease_topalbums.value = res.data
+	} catch (error) {
+		useToast().open({
+			message: error.message,
+			type: 'error',
+			duration: 3000,
+			dismissible: true,
+			position: 'top-right',
+		})
+	}
 }
 // get netease user recommand playlist
-const netease_daily = ref([]) as [Playlist]
+const netease_daily: Ref<Playlist[]> = ref([])
 const getNeteaseDaily = async () => {
-  // get netease user cookie 
-  const cookie = localStorage.getItem('netease-cookie')
-  if (!cookie) return
-  try {
-    const [res, err] = await handlePromise(neteaseRecommendDaily())
-    if (err) {
-      useToast().open({
-        message: err.message || '获取网易日推失败',
-        type: 'error',
-        duration: 3000,
-        dismissible: true,
-        position: 'top-right'
-      })
-      return
-    }
-    netease_daily.value = res.data
-  } catch (error) {
-    useToast().open({
-      message: error.message,
-      type: 'error',
-      duration: 3000,
-      dismissible: true,
-      position: 'top-right'
-    })
-  }
+	// get netease user cookie
+	const cookie = localStorage.getItem('netease-cookie')
+	if (!cookie) return
+	try {
+		const [res, err] = await handlePromise(
+			neteaseRecommendDaily({
+				cookie,
+			})
+		)
+		if (err) {
+			useToast().open({
+				message: err.message || '获取网易日推失败',
+				type: 'error',
+				duration: 3000,
+				dismissible: true,
+				position: 'top-right',
+			})
+			return
+		}
+		netease_daily.value = res.data
+	} catch (error) {
+		useToast().open({
+			message: error.message,
+			type: 'error',
+			duration: 3000,
+			dismissible: true,
+			position: 'top-right',
+		})
+	}
 }
 // toplaylist
 const toPlaylist = (id: number, type: string) => {
-  if (!type) {
-    router.push({
-      path: `playlist/${id}`
-    })
-    return
-  }
-  if (type === 'netease') {
-    router.push({
-      path: `netease-playlist/${id}`
-    })
-    return
-  }
-  if (type === 'netease-album') {
-    router.push({
-      path: `netease-album/${id}`
-    })
-    return
-  }
-  if (type === 'netease-daily') {
-    router.push({
-      path: `netease-daily/${id}`
-    })
-    return
-  }
-  if (type === 'favourites') {
-    router.push({
-      path: 'favourites/'
-    })
-    return
-  }
-  router.push({
-    path: 'netease-daily-tracks/'
-  })
+	if (!type) {
+		router.push({
+			path: `playlist/${id}`,
+		})
+		return
+	}
+	if (type === 'netease') {
+		router.push({
+			path: `netease-playlist/${id}`,
+		})
+		return
+	}
+	if (type === 'netease-album') {
+		router.push({
+			path: `netease-album/${id}`,
+		})
+		return
+	}
+	if (type === 'netease-daily') {
+		router.push({
+			path: `netease-daily/${id}`,
+		})
+		return
+	}
+	if (type === 'favourites') {
+		router.push({
+			path: 'favourites/',
+		})
+		return
+	}
+	router.push({
+		path: 'netease-daily-tracks/',
+	})
 }
 onMounted(async () => {
-  await homedata()
-  await getNeteaseTopAlbums()
-  await getNeteaseDaily()
+	await homedata()
+	await getNeteaseTopAlbums()
+	await getNeteaseDaily()
 })
 </script>
 
-<style lang="scss" scoped>
-// 设置scrollbar背景颜色
-::-webkit-scrollbar {
-  background-color: #f5f5f5;
-  width: 8px;
-}
-
-// 不显示scrollbar的轨道
-::-webkit-scrollbar-track {
-  background-color: #f5f5f5;
-}
-</style>
+<style lang="scss" scoped></style>
